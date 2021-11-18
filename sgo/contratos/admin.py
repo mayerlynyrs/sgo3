fd0c7e36206fc627ff69ac07b880188676394ec2
Nicolas Rojas <nrojas@empresasintegra.cl>
2021-11-18 16:49:28 -0300

    modulo de contratos terminado

--- sgo/contratos/admin.py
+++ sgo/contratos/admin.py
@@ -8,29 +8,78 @@ from import_export import resources, fields
 from import_export.widgets import ForeignKeyWidget
 from import_export.admin import ImportExportModelAdmin
 #Models
-from contratos.models import Plantilla, Contrato, DocumentosContrato, Tipo
+from contratos.models import Plantilla, Contrato, DocumentosContrato, TipoDocumento, Finiquito , ContratosBono, ContratosEquipo, Renuncia, Anexo , Revision
 
-
-class TipoSetResource(resources.ModelResource):
+class RenunciaSetResource(resources.ModelResource):
 
     class Meta:
-        model = Tipo
-        fields = ('id', 'nombre', )
+        model = Renuncia
+        fields = ('id', 'url','fecha_termino','status', )
 
+class ContratoInLine(admin.TabularInline):
 
+    class Meta:
+        model = Contrato
+        fields = ('id', 'sueldo_base','fecha_pago', 'fecha_inicio','fecha_termino' ,'fecha_termino_adendum' , 'url' ,'motivo', 'archivado',
+        'tipo_contrato','seguro_vida','estado_firma','estado_contrato','fecha_solicitud','fecha_solicitud_baja',
+        'fecha_aprobacion','fecha_aprobacion_baja','status', )
+
+class AnexoInLine(admin.TabularInline):
+
+    class Meta:
+        model = Anexo
+        fields = ('id', 'url','motivo', 'fecha_inicio','fecha_termino_anexo_anterior' ,'fecha_termino' , 'otroanexo','estado_firma',
+        'estado_anexo','fecha_solicitud','fecha_solicitud_baja','fecha_aprobacion','fecha_aprobacion_baja','status', )
+
+class TipoDocumentoSetResource(resources.ModelResource):
+
+    class Meta:
+        model = TipoDocumento
+        fields = ('id', 'nombre','status', )
+    
 class DocumentoContratoInLine(admin.TabularInline):
     model = DocumentosContrato
-    fields = ('archivo',)
+    fields = ('archivo', 'status')
     extra = 1
+
+class TipoDocumentoSetResource(resources.ModelResource):
+
+    class Meta:
+        model = TipoDocumento
+        fields = ('id', 'valor', 'descripcion','status', )
+
+class ContratosBonoSetResource(resources.ModelResource):
+
+    class Meta:
+        model = ContratosBono
+        fields = ('id', 'nombre','status', )
+
+class FiniquitoSetResource(resources.ModelResource):
+
+    class Meta:
+        model = Finiquito
+        fields = ('id', 'total_pagar','status', )
+
+class ContratosEquipoSetResource(resources.ModelResource):
+
+    class Meta:
+        model = ContratosEquipo
+        fields = ('id', 'cantidad','status', )
+
+class RevisionSetResource(resources.ModelResource):
+
+    class Meta:
+        model = Revision
+        fields = ('id', 'estado','obs','status', )
 
 @admin.register(Plantilla)
 class PlantillaAdmin(admin.ModelAdmin):
     """PlantillaAdmin model Admin."""
 
-    fields = ('nombre', 'tipo', 'archivo', 'clientes', 'plantas', 'activo')
+    fields = ('nombre', 'tipo', 'archivo', 'clientes', 'negocios', 'activo')
     list_display = ('id', 'nombre', 'tipo', 'clientes_list', 'plantas_list', 'activo', 'modified_by', )
     # list_display = ('codigo', 'nombre', 'tipo', 'cliente', 'plantas_list', 'activo', 'modified_by', 'modified', )
-    list_filter = ['clientes', 'plantas', ]
+    list_filter = ['clientes', 'negocios', ]
     search_fields = ('id', 'nombre', 'tipo', 'clientes_razon_social', 'plantas_nombre', )
 
     def clientes_list(self, obj):
@@ -44,12 +93,31 @@ class PlantillaAdmin(admin.ModelAdmin):
 class ContratoAdmin(admin.ModelAdmin):
     """ContratoAdmin model Admin."""
 
-    fields = ('usuario', 'estado', 'obs', 'archivado')
-    list_display = ('usuario', 'plantas_list', 'estado', 'archivado', 'modified', 'created_by')
+    fields = ('sueldo_base','fecha_pago', 'fecha_inicio','fecha_termino' ,'fecha_termino_adendum' , 'url' ,'motivo', 'archivado',
+        'tipo_contrato','seguro_vida','estado_firma','estado_contrato','fecha_solicitud','fecha_solicitud_baja',
+        'fecha_aprobacion','fecha_aprobacion_baja','usuario','gratificacion','horario','negocio','renuncia','status',)
+    list_display = ('id', 'sueldo_base','fecha_pago', 'fecha_inicio','fecha_termino' ,'fecha_termino_adendum' , 'url' ,'motivo', 'archivado',
+        'tipo_contrato','seguro_vida','estado_firma','estado_contrato','fecha_solicitud','fecha_solicitud_baja',
+        'fecha_aprobacion','fecha_aprobacion_baja',)
     #list_filter = ['usuario__planta', ]
     search_fields = ('usuario__rut', 'usuario__last_name', 'usuario__first_name',)
 
-    inlines = [DocumentoContratoInLine]
+    
+
+    def plantas_list(self, obj):
+        return u", ".join(o.nombre for o in obj.usuario.planta.all())
+
+@admin.register(Anexo)
+class AnexoAdmin(admin.ModelAdmin):
+    """AnexoAdmin model Admin."""
+
+    fields = ('url','motivo', 'fecha_inicio','fecha_termino_anexo_anterior' ,'fecha_termino' , 'otroanexo','estado_firma','estado_anexo',
+    'fecha_solicitud','fecha_solicitud_baja','fecha_aprobacion','fecha_aprobacion_baja','usuario','contrato','renuncia','negocio','status')
+    list_display = ('id', 'motivo', 'fecha_inicio','fecha_termino_anexo_anterior' ,'fecha_termino' , 'otroanexo','estado_firma','estado_anexo',
+    'fecha_solicitud','fecha_solicitud_baja','fecha_aprobacion','fecha_aprobacion_baja','status')
+    #list_filter = ['usuario__planta', ]
+    search_fields = ('usuario__rut', 'usuario__last_name', 'usuario__first_name',)
+
 
     def plantas_list(self, obj):
         return u", ".join(o.nombre for o in obj.usuario.planta.all())
@@ -58,7 +126,7 @@ class ContratoAdmin(admin.ModelAdmin):
 class DocumentoContrato(admin.ModelAdmin):
     """DocumentoContratoAdmnin model Admin."""
 
-    fields = ('contrato', 'archivo', )
+    fields = ('contrato', 'tipo_documento','url', 'status' )
     list_display = ('contrato_usuario', 'modified')
     search_fields = ('contrato', )
 
@@ -66,11 +134,57 @@ class DocumentoContrato(admin.ModelAdmin):
         return str(obj.contrato.usuario) + '-' + obj.nombre_archivo
 
 
-@admin.register(Tipo)
-class TipoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
-    """TipoAdmin model admin."""
+@admin.register(TipoDocumento)
+class TipoDocumentoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """TipoDocumentoAdmin model admin."""
 
-    resource_class = TipoSetResource
-    fields = ('nombre', )
+    resource_class = TipoDocumentoSetResource
+    fields = ('nombre', 'status', )
     list_display = ('id', 'nombre', 'created',)
+    search_fields = ['nombre', ]
+
+
+@admin.register(Finiquito)
+class FiniquitoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """FiniquitoAdmin model admin."""
+
+    resource_class = FiniquitoSetResource
+    fields = ('total_pagar','contrato', 'status', )
+    list_display = ('id', 'total_pagar', )
+    search_fields = ['nombre', ]
+
+@admin.register(ContratosBono)
+class ContratosBonoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """ContratosBonoAdmin model admin."""
+
+    resource_class = ContratosBonoSetResource
+    fields = ('Valor','descripcion', 'contrato', 'bono', 'status', )
+    list_display = ('id', 'descripcion' ,'Valor', )
+    search_fields = ['nombre', ]
+
+@admin.register(ContratosEquipo)
+class ContratosEquipoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """ContratosEquipoAdmin model admin."""
+
+    resource_class = ContratosEquipoSetResource
+    fields = ('cantidad', 'contrato', 'equipo', 'status', )
+    list_display = ('id', 'cantidad' , )
+    search_fields = ['nombre', ]
+
+@admin.register(Renuncia)
+class RenunciaAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """RenunciaAdmin model admin."""
+
+    resource_class = RenunciaSetResource
+    fields = ('url','fecha_termino','status' )
+    list_display = ('id', 'url', 'fecha_termino',)
+    search_fields = ['nombre', ]
+
+@admin.register(Revision)
+class RevisionAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    """RevisionAdmin model admin."""
+
+    resource_class = RevisionSetResource
+    fields = ( 'estado','obs','contrato','anexo','status', )
+    list_display = ('id', 'estado','obs','contrato','anexo','status',)
     search_fields = ['nombre', ]
--- sgo/contratos/models/contratos.py
+++ sgo/contratos/models/contratos.py
@@ -3,12 +3,33 @@ import os
 
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils import timezone
 from django.core.validators import FileExtensionValidator
 # Models
 from users.models import User
 # Utilities
-from utils.models import BaseModel
+from utils.models import BaseModel, Bono , Equipo , Gratificacion , Horario ,Negocio
+from contratos.models import TipoDocumento
+#from requerimientos.models import Requerimiento_user, Causal
+
 User = get_user_model()
+
+class Renuncia(BaseModel):
+    nombre = models.CharField(max_length=250)
+    url = models.FileField(
+        upload_to='renuncias/',
+        validators=[FileExtensionValidator(allowed_extensions=['doc', 'docx', ])]
+    )
+    fecha_termino = models.DateTimeField(blank=True, null=True)
+    
+
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el bono, deshabilite esta casilla.'
+    )
+    
+    def __str__(self):
+        return self.nombre
 
 
 class Contrato(BaseModel):
@@ -18,7 +39,17 @@ class Contrato(BaseModel):
     FIRMADO = 'FF'
     OBJETADO = 'OB'
 
-    CONTRATO_ESTADO = (
+    CREADO = 'CR'
+    PROCESO_VALIDACION = 'PV'
+    APROBADO = 'AP'
+    PENDIENTE_BAJA ='PB'
+    BAJADO = 'BJ'
+
+    DIARIO = 'D'
+    MENSUAL = 'M'
+    MENSUAL30 = 'M3'
+
+    FIRMA_ESTADO = (
         (POR_FIRMAR, 'Por Firmar'),
         (FIRMADO_TRABAJADOR, 'Firmado por Trabajador'),
         (FIRMADO_EMPLEADOR, 'Firmado por Empleador'),
@@ -26,11 +57,119 @@ class Contrato(BaseModel):
         (OBJETADO, 'Objetado'),
     )
 
+    CONTRATO_ESTADO = (
+        (CREADO, 'Creado'),
+        (PROCESO_VALIDACION, 'en Proceso de  Validacion'),
+        (APROBADO, 'Aprobado'),
+        (PENDIENTE_BAJA,'en Proceso de baja'),
+        (BAJADO,'Bajado'),
+    )
+
+    TIPO_CONTRATO = (
+        (DIARIO, 'Diario'),
+        (MENSUAL, 'Mensual'),
+        (MENSUAL30, 'Mensual 30 hrs'),
+    )
+
+    sueldo_base = models.IntegerField()
+    fecha_pago = models.DateTimeField(blank=True, null=True)
+    fecha_inicio = models.DateTimeField(blank=False, null=False)
+    fecha_termino = models.DateTimeField(blank=False, null=False)
+    fecha_termino_adendum = models.DateTimeField(blank=True, null=True)
+    url = models.FileField(
+        upload_to='contratoscreados/',
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'png', 'jpeg', 'jpg', ])]
+    )
+    motivo = models.TextField(blank=True, null=True)
     archivado = models.BooleanField(default=False)
-    estado = models.CharField(max_length=2, choices=CONTRATO_ESTADO, default=POR_FIRMAR)
+    tipo_contrato = models.CharField(max_length=2, choices=TIPO_CONTRATO, default=MENSUAL)
+    seguro_vida = models.BooleanField(
+        default=False,
+        help_text='Para desactivar el seguro de vida, deshabilite esta casilla.'
+    )
+    estado_firma = models.CharField(max_length=2, choices=FIRMA_ESTADO, default=POR_FIRMAR)
+    estado_contrato = models.CharField(max_length=2, choices=CONTRATO_ESTADO, default=CREADO)
+    fecha_solicitud = models.DateTimeField(blank=True, null=True)
+    fecha_solicitud_baja = models.DateTimeField(blank=True, null=True)
+    fecha_aprobacion = models.DateTimeField(blank=True, null=True)
+    fecha_aprobacion_baja = models.DateTimeField(blank=True, null=True)
     obs = models.TextField(blank=True, null=True)
 
     usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+    gratificacion = models.ForeignKey(Gratificacion, on_delete=models.PROTECT)
+    horario = models.ForeignKey(Horario, on_delete=models.PROTECT)
+    renuncia = models.ForeignKey(Renuncia, on_delete=models.PROTECT)
+    negocio = models.ForeignKey(Negocio, on_delete=models.PROTECT) 
+    # requerimiento_user = models.ForeignKey(Requerimiento_user, on_delete=models.PROTECT)
+    # causal = models.ForeignKey(Causal, on_delete=models.PROTECT)
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el los equipos de este contrato, deshabilite esta casilla.'
+    )
+    def __str__(self):
+        return str(self.usuario.rut) + '-' +str(self.id).zfill(4)
+
+
+def contrato_directory_path(instance, filename):
+    return '/'.join(['contratos', str(instance.contrato.usuario.id), filename])
+
+class Anexo(BaseModel):
+    POR_FIRMAR = 'PF'
+    FIRMADO_TRABAJADOR = 'FT'
+    FIRMADO_EMPLEADOR = 'FE'
+    FIRMADO = 'FF'
+    OBJETADO = 'OB'
+
+    CREADO = 'CR'
+    PROCESO_VALIDACION = 'PV'
+    APROBADO = 'AP'
+    PENDIENTE_BAJA ='PB'
+    BAJADO = 'BJ'
+
+    FIRMA_ESTADO = (
+        (POR_FIRMAR, 'Por Firmar'),
+        (FIRMADO_TRABAJADOR, 'Firmado por Trabajador'),
+        (FIRMADO_EMPLEADOR, 'Firmado por Empleador'),
+        (FIRMADO, 'Firmado'),
+        (OBJETADO, 'Objetado'),
+    )
+
+    ANEXO_ESTADO = (
+        (CREADO, 'Creado'),
+        (PROCESO_VALIDACION, 'en Proceso de  Validacion'),
+        (APROBADO, 'Aprobado'),
+        (PENDIENTE_BAJA,'en Proceso de baja'),
+        (BAJADO,'Bajado'),
+    )
+
+    
+    url = models.FileField(
+        upload_to='anexoscreados/',
+        validators=[FileExtensionValidator(allowed_extensions=['pdf', 'png', 'jpeg', 'jpg', ])]
+    )
+    motivo = models.TextField(blank=True, null=True)
+    fecha_inicio = models.DateTimeField(blank=False, null=False)
+    fecha_termino_anexo_anterior = models.DateTimeField(blank=False, null=False)
+    fecha_termino = models.DateTimeField(blank=False, null=False)
+    otroanexo = models.BooleanField(default=False)
+    estado_firma = models.CharField(max_length=2, choices=FIRMA_ESTADO, default=POR_FIRMAR)
+    estado_anexo = models.CharField(max_length=2, choices=ANEXO_ESTADO, default=CREADO)
+    fecha_solicitud = models.DateTimeField(blank=True, null=True)
+    fecha_solicitud_baja = models.DateTimeField(blank=True, null=True)
+    fecha_aprobacion = models.DateTimeField(blank=True, null=True)
+    fecha_aprobacion_baja = models.DateTimeField(blank=True, null=True)
+    obs = models.TextField(blank=True, null=True)
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el anexo, deshabilite esta casilla.'
+    )
+    
+    usuario = models.ForeignKey(User, on_delete=models.PROTECT)
+    contrato = models.ForeignKey(Contrato, on_delete=models.PROTECT)
+    renuncia = models.ForeignKey(Renuncia, on_delete=models.PROTECT)
+    negocio = models.ForeignKey(Negocio, on_delete=models.PROTECT) 
+    # requerimiento_user = models.ForeignKey(Requerimiento_user, on_delete=models.PROTECT)
+    # causal = models.ForeignKey(Causal, on_delete=models.PROTECT)
 
     def __str__(self):
         return str(self.usuario.rut) + '-' +str(self.id).zfill(4)
@@ -39,12 +178,16 @@ class Contrato(BaseModel):
 def contrato_directory_path(instance, filename):
     return '/'.join(['contratos', str(instance.contrato.usuario.id), filename])
 
-
 class DocumentosContrato(BaseModel):
-    archivo = models.FileField(upload_to=contrato_directory_path,
+    url = models.FileField(upload_to=contrato_directory_path,
                                validators=[
                                    FileExtensionValidator(allowed_extensions=['pdf', ])])
     contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+    tipo_documento = models.ForeignKey(TipoDocumento, on_delete=models.CASCADE)
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el Documento, deshabilite esta casilla.'
+    )
 
     class Meta:
         ordering = ['contrato']
@@ -57,3 +200,80 @@ class DocumentosContrato(BaseModel):
     @property
     def nombre_archivo(self):
         return os.path.basename(self.archivo.name)
+
+
+class ContratosBono(models.Model):
+    Valor = models.IntegerField()
+    descripcion = models.TextField()
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+    bono = models.ForeignKey(Bono, on_delete=models.CASCADE)
+
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el bono, deshabilite esta casilla.'
+    )
+    created_date = models.DateTimeField(
+        default= timezone.now,
+        null=True,
+        blank=True
+    )
+    def __str__(self):
+        return self.nombre
+
+
+class Finiquito(BaseModel):
+    total_pagar = models.IntegerField()
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el finiquito, deshabilite esta casilla.'
+    )
+    created_date = models.DateTimeField(
+        default= timezone.now,
+        null=True,
+        blank=True
+    )
+    def __str__(self):
+        return self.nombre
+
+class ContratosEquipo(BaseModel):
+    cantidad = models.IntegerField()
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+    equipo = models.ForeignKey(Equipo, on_delete=models.CASCADE)
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el los equipos de este contrato, deshabilite esta casilla.'
+    )
+    created_date = models.DateTimeField(
+        default= timezone.now,
+        null=True,
+        blank=True
+    )
+    def __str__(self):
+        return self.nombre
+
+class Revision(BaseModel):
+
+    PENDIENTE = 'PD'
+    APROBADO = 'AP'
+    RECHAZADO ='RC'
+  
+
+    ESTADO = (
+        (PENDIENTE, 'Pendiente'),
+        (APROBADO, 'Aprobado'),
+        (RECHAZADO,'Rechazado'),
+    )
+    obs = models.TextField(blank=True, null=True)
+    estado = models.CharField(max_length=2, choices=ESTADO, default=PENDIENTE)
+    contrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)
+    anexo = models.ForeignKey(Anexo, on_delete=models.CASCADE)
+    status = models.BooleanField(
+        default=True,
+        help_text='Para desactivar el los equipos de este contrato, deshabilite esta casilla.'
+    )
+    def __str__(self):
+        return self.nombre
+
+    
